3dd01abc637c7721fdfe245fc9ec2af0673cd126
Wes Hampson <thehambone93@gmail.com>
2020-10-21 22:28:09 -0700

    Add SaveSlot

---
+++ src/GTA3SaveEditor.GUI/SaveSlot.cs
@@ -0,0 +1,29 @@
+﻿using WpfEssentials;
+
+namespace GTA3SaveEditor.GUI
+{
+    public class SaveSlot : ObservableObject
+    {
+        private string m_name;
+        private string m_path;
+        private bool m_inUse;
+
+        public string Name
+        {
+            get { return m_name; }
+            set { m_name = value; OnPropertyChanged(); }
+        }
+
+        public string Path
+        {
+            get { return m_path; }
+            set { m_path = value; OnPropertyChanged(); }
+        }
+
+        public bool InUse
+        {
+            get { return m_inUse; }
+            set { m_inUse = value; OnPropertyChanged(); }
+        }
+    }
+}
