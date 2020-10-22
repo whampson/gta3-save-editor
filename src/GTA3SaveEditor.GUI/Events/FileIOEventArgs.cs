d31585bea9f1a4cdaede3006a4afa39e51d627c0
Wes Hampson <thehambone93@gmail.com>
2020-10-21 22:27:51 -0700

    Add FileIOEventArgs

---
+++ src/GTA3SaveEditor.GUI/Events/FileIOEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GTA3SaveEditor.GUI.Events
+{
+    public class FileIOEventArgs : EventArgs
+    {
+        public string Path { get; set; }
+        public bool SuppressDialogs { get; set; }
+    }
+}
