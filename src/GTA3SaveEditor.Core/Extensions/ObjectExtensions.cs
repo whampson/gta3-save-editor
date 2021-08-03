4f74a0a88560e6154ba8b72803b404fa4b2e6472
Wes Hampson <thehambone93@gmail.com>
2021-08-02 22:07:31 -0700

    Add some Class Extensions

---
+++ src/GTA3SaveEditor.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GTA3SaveEditor.Core.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNumericType(this object o)
+        {
+            if (IsIntegerType(o)) return true;
+
+            switch (Type.GetTypeCode(o.GetType()))
+            {
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Single:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsIntegerType(this object o)
+        {
+            if (o == null) return false;
+
+            switch (Type.GetTypeCode(o.GetType()))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ src/GTA3SaveEditor.GUI/Extensions/ColorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using MColor = System.Windows.Media.Color;
+using DColor = System.Drawing.Color;
+
+namespace GTA3SaveEditor.GUI.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static MColor ToMediaColor(this DColor color)
+        {
+            return MColor.FromArgb(color.A, color.R, color.G, color.B);
+        }
+
+        public static DColor ToDrawingColor(this MColor color)
+        {
+            return DColor.FromArgb(color.A, color.R, color.G, color.B);
+        }
+    }
+}
