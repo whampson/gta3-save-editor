e5ea0591864df4d1e80b58df4ac0e779201ed53f
Wes Hampson <thehambone93@gmail.com>
2021-08-02 22:05:49 -0700

    Add some converters

---
+++ src/GTA3SaveEditor.GUI/Converters/CarColorConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using GTA3SaveEditor.Core.Game;
+using GTA3SaveEditor.GUI.Extensions;
+
+namespace GTA3SaveEditor.GUI.Converters
+{
+    [ValueConversion(typeof(byte), typeof(System.Windows.Media.Color))]
+    public class CarColorConverter : ConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is byte c)
+            {
+                var colorInfo = GTA3.CarColors.ElementAtOrDefault(c);
+                if (colorInfo == null)
+                {
+                    return Colors.Black;
+                }
+                return colorInfo.Color.ToMediaColor();
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color color)
+            {
+                var c = GTA3.CarColors.FirstOrDefault(x => x.Color.ToMediaColor() == color);
+                if (c == default)
+                {
+                    return (byte) 0;
+                }
+
+                return (byte) GTA3.CarColors.ToList().IndexOf(c);
+            }
+
+            throw new NotSupportedException($"Cannot convert '{value}' to type {targetType}.");
+        }
+    }
+}
---
+++ src/GTA3SaveEditor.GUI/Converters/EnumFlagsConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace GTA3SaveEditor.GUI.Converters
+{
+    public class EnumFlagsConverter : ConverterBase
+    {
+        private Enum m_target;
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum e && parameter is Enum flag)
+            {
+                m_target = e;
+                return e.HasFlag(flag);
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool v && parameter is Enum flag)
+            {
+                int e = EnumToInt(m_target);
+                int f = EnumToInt(flag);
+
+                int result = (v) ? (e | f) : (e & ~f);
+
+                Enum retval = (Enum) Enum.Parse(targetType, result.ToString());
+                return retval;
+            }
+            throw new NotSupportedException($"Cannot convert '{value}' to type {targetType}.");
+        }
+
+        private static int EnumToInt(Enum e) => (int) (object) e;
+    }
+}
